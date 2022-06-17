6109361c44ecd47e6e1267de6df13e4f1965fdda
BadalRaj16 <a7258062359b@gmail.com>
2022-06-17 11:48:48 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
     
       <div className='w-[25rem] h-[15rem]  flex flex-col gap-5 p-10 justify-around text-center'>
         <div className='text-3xl font-bold mb-5'>Log in to XYZ</div>
-        <button className='bg-gray-800 p-3 rounded-xl hover:bg-gray-700 hover:zoom '>Github</button>
-        <button className='bg-red-500 p-3 rounded-xl hover:bg-red-400'>Google</button>
-        <button className='bg-blue-700 p-3 rounded-xl hover:bg-blue-600'>LinkedIn</button>
+        <button className='bg-gray-800 p-3 rounded-xl hover:bg-gray-700 hover:scale-[1.05] duration-500 '>Github</button>
+        <button className='bg-red-500 p-3 rounded-xl hover:bg-red-400 hover:scale-[1.05] duration-500'>Google</button>
+        <button className='bg-blue-700 p-3 rounded-xl hover:bg-blue-600 hover:scale-[1.05]  duration-500'>LinkedIn</button>
       </div>
       
 
