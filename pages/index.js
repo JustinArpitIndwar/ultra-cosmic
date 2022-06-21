b3c2e80a88884534684c8cda5edc317aa20c994f
AbhishekPurty <abhishekpurty1999@gmail.com>
2022-06-21 21:32:36 +0530

    icon

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,10 @@ export default function Home() {
     <div className='h-screen flex justify-center place-content-center p-52 '>
     
       <div className='w-[25rem] h-[15rem]  flex flex-col gap-5 p-10 justify-around text-center'>
-        <div className='text-3xl font-bold mb-5'>Log in to XYZ</div>
-        <button className='bg-gray-800 p-3 rounded-xl hover:bg-gray-700 hover:scale-[1.05] duration-500 '>Github</button>
-        <button className='bg-red-500 p-3 rounded-xl hover:bg-red-400 hover:scale-[1.05] duration-500'>Google</button>
-        <button className='bg-blue-700 p-3 rounded-xl hover:bg-blue-600 hover:scale-[1.05]  duration-500'>LinkedIn</button>
+        <div className='text-3xl font-bold mb-5'>Log in to CodeQuake</div>
+        <button onClick={() => signIn('github')} className='bg-gray-800 p-3 rounded-xl hover:bg-gray-700 hover:scale-[1.05] duration-500 '>Github</button>
+        <button onClick={() => signIn('google')} className='bg-red-500 p-3 rounded-xl hover:bg-red-400 hover:scale-[1.05] duration-500'>Google</button>
+        <button onClick={() => signIn('linkedin')} className='bg-blue-700 p-3 rounded-xl hover:bg-blue-600 hover:scale-[1.05]  duration-500'>LinkedIn</button>
       </div>
       
 
