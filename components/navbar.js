515677efadb2f08a71c817fd53ec101a448ce2ae
AbhishekPurty <abhishekpurty1999@gmail.com>
2022-07-05 23:06:00 +0530

    first prototype host

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,4 @@
-
+import Link from "next/link"
 
 export default function Navbar() {
     return (
@@ -6,18 +6,19 @@ export default function Navbar() {
         <div className = "top">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
             <div className="topLeft">
-                
+                <Link href="/">
                 <img
                     className="LogoImg"
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_xiCAwX4JHf4O_hDoeBQTuV6r-hXviCbeEA&usqp=CAU"
                     alt="Logo"
                 />
+                </Link>
             </div>
             
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">Home</li>
-                    <li className="topListItem">Practice</li>
+                    <li className="topListItem"><Link href="/home"><a target="_blank">Home</a></Link></li>
+                    <li className="topListItem"> Practice</li>
                     <li className="topListItem">Compete</li>
                 </ul>
             </div>
